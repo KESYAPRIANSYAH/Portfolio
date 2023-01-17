a809e4a58173667440a4217273a3ca6bb77f34fb
username_anda <kesyapriansyah@gmail.com>
2023-01-17 16:49:52 +0700

    first commit

---
+++ Script.js
@@ -0,0 +1,24 @@
+// navbar js
+
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+
+  // navbar js
+});
+
+// dark darkMode
+let body = document.body;
+// inisialisasi fungction
+function darkMode() {
+  console.log("dark mode di klik");
+  body.classList.toggle("dark");
+}
+
+// dark darkMode
+
+// sliders JS
+
+// sliders JS
